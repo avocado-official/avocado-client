a15daaaa84c430ad5ed1b73ebf2598f946bd0885
Matin Kaboli <matinkaboli@aol.com>
2021-06-25 03:24:03 +0430

    fix: ant design rtl

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,20 @@
-import '../styles/globals.css'
-import 'bootstrap/dist/css/bootstrap.css'
-import { Provider } from 'react-redux'
-import { useStore } from '../store'
+import '../styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import { Provider } from 'react-redux';
+import { useStore } from '../store';
+import { ConfigProvider } from 'antd';
 
 function MyApp({ Component, pageProps }) {
-  const store = useStore(pageProps.initialReduxState)
+  const store = useStore(pageProps.initialReduxState);
 
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <ConfigProvider direction="rtl">
+        <Component {...pageProps} />
+      </ConfigProvider>
     </Provider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/signup.js
+++ pages/signup.js
@@ -1,60 +1,54 @@
-import React from 'react'
-import { useForm } from 'react-hook-form'
-import styles from '../styles/signup.module.css'
-import { Row, Col } from 'antd'
-import Error from '../components/Error'
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import styles from '../styles/signup.module.css';
+import { Row, Col } from 'antd';
+import Error from '../components/Error';
 
 export default function signup() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm()
+  } = useForm();
 
-  const onSubmit = (data) => console.log(data)
+  const onSubmit = (data) => console.log(data);
 
   return (
     <Row>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className={styles.form}
-        autocomplete='off'
-      >
-        <Col>
+      <Col xs={{ span: 22, offset: 1 }} sm={{ span: 12, offset: 6 }}>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className={styles.form}
+          autocomplete="off"
+        >
           <input
             className={styles.input}
             {...register('name', { required: true, maxLength: 20 })}
-            placeholder='نام'
+            placeholder="نام"
           />
-          {errors.name?.type === 'required' && (
-            <Error field='نام' type='required' />
-          )}
-          {errors.name?.type === 'maxLength' && (
-            <Error field='نام' type='maxLenght' />
-          )}
-        </Col>
-        <Col>
+          {errors.name?.type === 'required' && <Error field="نام" type="required" />}
+          {errors.name?.type === 'maxLength' && <Error field="نام" type="maxLenght" />}
+
           <input
             className={styles.input}
-            type='tel'
+            type="tel"
             {...register('phone', {
               required: true,
+              pattern: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im,
               maxLength: 11,
-              minLength: 11,
             })}
-            placeholder='تلفن همراه'
+            placeholder="تلفن همراه"
           />
           {errors.phone?.type === 'required' && (
-            <Error field='تلفن همراه' type='required' />
+            <Error field="تلفن همراه" type="required" />
+          )}
+          {errors.phone?.type === 'pattern' && (
+            <Error field="تلفن همراه" type="pattern" />
           )}
           {errors.phone?.type === 'maxLength' && (
-            <Error field='تلفن همراه' type='maxLenght' />
+            <Error field="تلفن همراه" type="pattern" />
           )}
-          {errors.phone?.type === 'maxLength' && (
-            <Error field='تلفن همراه' type='minLength' />
-          )}
-        </Col>
-        <Col>
+
           <input
             className={styles.input}
             {...register('password', {
@@ -62,22 +56,21 @@ export default function signup() {
               maxLength: 18,
               minLength: 7,
             })}
-            placeholder='رمز عبور'
+            placeholder="رمز عبور"
           />
           {errors.password?.type === 'required' && (
-            <Error field='رمز عبور' type='required' />
+            <Error field="رمز عبور" type="required" />
           )}
           {errors.password?.type === 'maxLength' && (
-            <Error field='رمز عبور' type='maxLength' />
+            <Error field="رمز عبور" type="maxLength" />
           )}
           {errors.password?.type === 'minLength' && (
-            <Error field='رمز عبور' type='minLength' />
+            <Error field="رمز عبور" type="minLength" />
           )}
-        </Col>
-        <Col>
-          <input className={styles.button} type='submit' value='ثبت نام' />
-        </Col>
-      </form>
+
+          <input className={styles.button} type="submit" value="ثبت نام" />
+        </form>
+      </Col>
     </Row>
-  )
+  );
 }
