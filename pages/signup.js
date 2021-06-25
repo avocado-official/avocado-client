8ff35ef3ded751868df0b0a5409bc7dfe46aeb27
shirinapr <shirinapr1@gmail.com>
2021-06-25 05:24:01 +0430

    final signup

--- pages/signup.js
+++ pages/signup.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import styles from '../styles/signup.module.css';
-import { Row, Col } from 'antd';
-import Error from '../components/Error';
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import styles from '../styles/signup.module.css'
+import { Row, Col } from 'antd'
+import Error from '../components/Error'
+import Link from 'next/link'
 
 export default function signup() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm()
 
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => console.log(data)
 
   return (
     <Row>
@@ -19,34 +20,39 @@ export default function signup() {
         <form
           onSubmit={handleSubmit(onSubmit)}
           className={styles.form}
-          autocomplete="off"
+          autocomplete='off'
         >
           <input
             className={styles.input}
             {...register('name', { required: true, maxLength: 20 })}
-            placeholder="نام"
+            placeholder='نام'
           />
-          {errors.name?.type === 'required' && <Error field="نام" type="required" />}
-          {errors.name?.type === 'maxLength' && <Error field="نام" type="maxLenght" />}
+          {errors.name?.type === 'required' && (
+            <Error field='نام' type='required' />
+          )}
+          {errors.name?.type === 'maxLength' && (
+            <Error field='نام' type='maxLenght' />
+          )}
 
           <input
             className={styles.input}
-            type="tel"
+            type='tel'
             {...register('phone', {
               required: true,
-              pattern: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im,
+              pattern:
+                /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im,
               maxLength: 11,
             })}
-            placeholder="تلفن همراه"
+            placeholder='تلفن همراه'
           />
           {errors.phone?.type === 'required' && (
-            <Error field="تلفن همراه" type="required" />
+            <Error field='تلفن همراه' type='required' />
           )}
           {errors.phone?.type === 'pattern' && (
-            <Error field="تلفن همراه" type="pattern" />
+            <Error field='تلفن همراه' type='pattern' />
           )}
           {errors.phone?.type === 'maxLength' && (
-            <Error field="تلفن همراه" type="pattern" />
+            <Error field='تلفن همراه' type='pattern' />
           )}
 
           <input
@@ -56,21 +62,34 @@ export default function signup() {
               maxLength: 18,
               minLength: 7,
             })}
-            placeholder="رمز عبور"
+            placeholder='رمز عبور'
           />
           {errors.password?.type === 'required' && (
-            <Error field="رمز عبور" type="required" />
+            <Error field='رمز عبور' type='required' />
           )}
           {errors.password?.type === 'maxLength' && (
-            <Error field="رمز عبور" type="maxLength" />
+            <Error field='رمز عبور' type='maxLength' />
           )}
           {errors.password?.type === 'minLength' && (
-            <Error field="رمز عبور" type="minLength" />
+            <Error field='رمز عبور' type='minLength' />
           )}
 
-          <input className={styles.button} type="submit" value="ثبت نام" />
+          <input className={styles.button} type='submit' value='ثبت نام' />
+          <div className={styles.loginLink}>
+            <Link href='/login'>
+              <a className={styles.aLink}>ورود</a>
+            </Link>
+          </div>
+          <div className={styles.terms}>
+            <p>با ثبت نام و ورود در این برنامه قوانین ما را تایید کرده اید.</p>
+          </div>
+          <div className={styles.problem}>
+            <Link href='/contactus'>
+              <a className={styles.problemLink}>گزارش مشکل</a>
+            </Link>
+          </div>
         </form>
       </Col>
     </Row>
-  );
+  )
 }
