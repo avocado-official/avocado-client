fa72fac96f988f7cd6bdd93e7644d0371600c74f
Amirrk79 <amirmntmntmntmnt@gmail.com>
2021-06-25 17:21:57 +0430

    fix: wrong push fixed

---
+++ pages/login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Row, Col } from 'antd';
+
+export default function Login() {
+  return (
+    <Row>
+      <Col span={6} offset={6}>
+        <p>Hello</p>
+      </Col>
+    </Row>
+  );
+}
--- pages/signup.js
+++ pages/signup.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
-import Input from '../components/Input'
-import Select from '../components/Select'
 import styles from '../styles/signup.module.css'
 import { Row, Col } from 'antd'
 import Error from '../components/Error'
+import Link from 'next/link'
 
 export default function signup() {
   const {
@@ -17,72 +16,80 @@ export default function signup() {
 
   return (
     <Row>
-      <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-        {/* <label className={styles.label}>نام</label> */}
-        <input
-          className={styles.input}
-          {...register('name', { required: true, maxLength: 20 })}
-          placeholder='نام'
-        />
-        {errors.name?.type === 'required' && (
-          <Error field='نام' type='required' />
-        )}
-        {errors.name?.type === 'maxLength' && (
-          <Error field='نام' type='maxLenght' />
-        )}
-        <input
-          className={styles.input}
-          type='tel'
-          {...register('phone', {
-            required: true,
-            maxLength: 11,
-            minLength: 11,
-          })}
-          placeholder='تلفن همراه'
-        />
-        {errors.phone?.type === 'required' && (
-          <Error field='تلفن همراه' type='required' />
-        )}
-        {errors.phone?.type === 'maxLength' && (
-          <Error field='تلفن همراه' type='maxLenght' />
-        )}
-        {errors.phone?.type === 'maxLength' && (
-          <Error field='تلفن همراه' type='minLength' />
-        )}
-
-        <input
-          className={styles.input}
-          {...register('password', {
-            required: true,
-            maxLength: 18,
-            minLength: 7,
-          })}
-          placeholder='رمز عبور'
-        />
-        {errors.password?.type === 'required' && 'رمز عبور الزامی است.'}
-        {errors.password?.type === 'maxLength' &&
-          'تعداد حروف بیشتر از حد مجاز است.'}
-        {errors.password?.type === 'minLength' &&
-          'تعداد حروف کمتر از حد مجاز است.'}
-
-        <select
-          className={styles.select}
-          {...register('city', { required: true })}
+      <Col xs={{ span: 22, offset: 1 }} sm={{ span: 12, offset: 6 }}>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className={styles.form}
+          autocomplete='off'
         >
-          <option selected>محل سکونت</option>
-          <option value='Tehran' className={styles.option}>
-            تهران
-          </option>
-          <option value='Esfahan' className={styles.option}>
-            اصفهان
-          </option>
-          <option value='Qom' className={styles.option}>
-            قم
-          </option>
-        </select>
+          <input
+            className={styles.input}
+            {...register('name', { required: true, maxLength: 20 })}
+            placeholder='نام'
+          />
+          {errors.name?.type === 'required' && (
+            <Error field='نام' type='required' />
+          )}
+          {errors.name?.type === 'maxLength' && (
+            <Error field='نام' type='maxLenght' />
+          )}
 
-        <input className={styles.button} type='submit' value='ثبت نام' />
-      </form>
+          <input
+            className={styles.input}
+            type='tel'
+            {...register('phone', {
+              required: true,
+              pattern:
+                /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im,
+              maxLength: 11,
+            })}
+            placeholder='تلفن همراه'
+          />
+          {errors.phone?.type === 'required' && (
+            <Error field='تلفن همراه' type='required' />
+          )}
+          {errors.phone?.type === 'pattern' && (
+            <Error field='تلفن همراه' type='pattern' />
+          )}
+          {errors.phone?.type === 'maxLength' && (
+            <Error field='تلفن همراه' type='pattern' />
+          )}
+
+          <input
+            className={styles.input}
+            {...register('password', {
+              required: true,
+              maxLength: 18,
+              minLength: 7,
+            })}
+            placeholder='رمز عبور'
+          />
+          {errors.password?.type === 'required' && (
+            <Error field='رمز عبور' type='required' />
+          )}
+          {errors.password?.type === 'maxLength' && (
+            <Error field='رمز عبور' type='maxLength' />
+          )}
+          {errors.password?.type === 'minLength' && (
+            <Error field='رمز عبور' type='minLength' />
+          )}
+
+          <input className={styles.button} type='submit' value='ثبت نام' />
+          <div className={styles.loginLink}>
+            <Link href='/login'>
+              <a className={styles.aLink}>ورود</a>
+            </Link>
+          </div>
+          <div className={styles.terms}>
+            <p>با ثبت نام و ورود در این برنامه قوانین ما را تایید کرده اید.</p>
+          </div>
+          <div className={styles.problem}>
+            <Link href='/contactus'>
+              <a className={styles.problemLink}>گزارش مشکل</a>
+            </Link>
+          </div>
+        </form>
+      </Col>
     </Row>
   )
 }
