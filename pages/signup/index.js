452e13e4a4b39ba9b6b45d43e74cc27fd56b9993
Matin Kaboli <matinkaboli@aol.com>
2021-06-27 01:38:00 +0430

    fix: merge

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
 import React from 'react';
 import Link from 'next/link';
 import { useForm } from 'react-hook-form';
-=======
-import React, { useState } from 'react'
-import { useForm, Controller } from 'react-hook-form'
-import styles from '../../styles/signup.module.css'
-import { Row, Col } from 'antd'
-import Error from '../../components/Error'
-import Link from 'next/link'
-import Input from '../../components/Input/index'
-import Select from 'react-select'
->>>>>>> a86b116ac58b12a49d2f52c37a69418fb80b0818
 
 import Error from '../../components/Error';
 import Input from '../../components/Input/index';
@@ -29,7 +18,7 @@ export default function Signup() {
   return (
     <div className="container">
       <div className="row justify-content-center">
-        <div className="col-10">
+        <div className="col-11 col-md-8">
           <form
             onSubmit={handleSubmit(onSubmit)}
             className={styles.form}
