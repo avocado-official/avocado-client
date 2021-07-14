94a01b1b709cdb537f18c72e3b5cd26b5b623647
shirinapr <shirinapr1@gmail.com>
2021-07-14 21:40:58 +0430

    props card added.

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,11 @@ export default function Home() {
       </Head>
       <Layout headerTitle='خانه'>
         <div>دیوار مهربانی آنلاین</div>
-        <Card />
+        <Card
+          img='https://lh3.googleusercontent.com/proxy/VfIQKMuF6bN864hL9BIvk1PaoIwD0pIJtNsQtZ0NwA_Y4E129wqXqYOxratIevZQUHGAkT0xjIFJr28Tiv8-K3suFhhu7uR1T47B1tQ3XQOaiWpVATcFhHUrVyAraAuiZscBVU4pcSSrx5w0DhYvb6zfw4gLKyF1Yi9-AWF8h9WB7UKj0dCll_8NJbhvBEPg_bySRU-ZB_nvMEAaQ03FDBRcGymf5H2ziO8v-w'
+          title='کاهو باغی'
+          distance='7km'
+        />
       </Layout>
     </div>
   )
