92ad4912873f6a164d7c9bf884f3743cc883a601
shirinapr <shirinapr1@gmail.com>
2021-07-17 03:30:42 +0430

    map page fixed

---
+++ components/Map/index.js
@@ -0,0 +1,52 @@
+import mapboxgl from 'mapbox-gl'
+import MapboxLanguage from '@mapbox/mapbox-gl-language'
+import React, { useRef, useEffect, useState } from 'react'
+
+import styles from '../../styles/map.module.scss'
+
+mapboxgl.accessToken =
+  'pk.eyJ1Ijoic2hpcmluYXByIiwiYSI6ImNrcjZvYjl4bTNocDMycG82d205enNwN24ifQ.tAomZwI2G5_9o9O9DD1D8w'
+
+export default function MapComp() {
+  const mapContainer = useRef(null)
+  const map = useRef(null)
+  const [lng, setLng] = useState(51.38)
+  const [lat, setLat] = useState(35.68)
+  const [zoom, setZoom] = useState(10)
+
+  useEffect(() => {
+    if (map.current) return
+
+    var language = new MapboxLanguage()
+
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom,
+    })
+
+    mapboxgl.setRTLTextPlugin(
+      'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.2.3/mapbox-gl-rtl-text.js'
+    )
+
+    map.current.addControl(language)
+
+    var marker = new mapboxgl.Marker({
+      color: '#21897e',
+      draggable: true,
+    })
+      .setLngLat([51.3022811, 35.7740779])
+      .addTo(map.current)
+  })
+
+  return (
+    <div className='container'>
+      <div className='row justify-content-center'>
+        <div className='col-12 col-md-8'>
+          <div ref={mapContainer} className={styles.mapContainer} />
+        </div>
+      </div>
+    </div>
+  )
+}
--- pages/map/index.js
+++ pages/map/index.js
@@ -1,53 +1,14 @@
-import mapboxgl from 'mapbox-gl';
-import MapboxLanguage from '@mapbox/mapbox-gl-language';
-import React, { useRef, useEffect, useState } from 'react';
+import Map from '../../components/Map'
+import Head from 'next/head'
+import Layout from '../../components/layout/layout'
 
-import styles from '../../styles/map.module.scss';
-
-mapboxgl.accessToken =
-  'pk.eyJ1Ijoic2hpcmluYXByIiwiYSI6ImNrcjZvYjl4bTNocDMycG82d205enNwN24ifQ.tAomZwI2G5_9o9O9DD1D8w';
-
-export default function Map() {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(51.38);
-  const [lat, setLat] = useState(35.68);
-  const [zoom, setZoom] = useState(10);
-
-  useEffect(() => {
-    if (map.current) return;
-
-    var language = new MapboxLanguage();
-
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      // style: 'mapbox://styles/mapbox/dark-v10',
-      center: [lng, lat],
-      zoom: zoom,
-    });
-
-    mapboxgl.setRTLTextPlugin(
-      'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.2.3/mapbox-gl-rtl-text.js',
-    );
-
-    map.current.addControl(language);
-
-    var marker = new mapboxgl.Marker({
-      color: '#FF0000',
-      draggable: true,
-    })
-      .setLngLat([51.3022811, 35.7740779])
-      .addTo(map.current);
-  });
-
+export default function MapComp() {
   return (
-    <div className="container">
-      <div className="row justify-content-center">
-        <div className="col-8">
-          <div ref={mapContainer} className={styles.mapContainer} />
-        </div>
-      </div>
+    <div>
+      <Head />
+      <Layout headerTitle='نقشه'>
+        <Map />
+      </Layout>
     </div>
-  );
+  )
 }
