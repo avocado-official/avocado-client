ebd54f852fadce5279c517566a8d56de2e321cc9
Shirin <shirinapr1@gmail.com>
2021-09-25 17:57:03 +0330

    dige commite maani dar nemizanam

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -14,37 +14,54 @@ const ProfileComp = ({ img }) => {
 				<div className={styles.picfather}>
 					<img className={styles.pic} src={img} />
 				</div>
-
-				<div className={styles.userInfo}>
-					<BsInfo className={styles.icon} />
-					<Link href="/user-info">
-						<a className={styles.userInfo}>اطلاعات کاربری</a>
-					</Link>
-					<IoChevronBack className={styles.back} />
-				</div>
-
+				<Link href='/user-info'>
+					<>
+						<div className={styles.box}>
+							<BsInfo className={styles.icon} />
+							<IoChevronBack className={styles.back} />
+							<a>اطلاعات کاربری</a>
+						</div>
+					</>
+				</Link>
 				<div className={cn(styles.khat, 'col-11 offset-1')} />
-
-				<div className={styles.setting}>
-					<RiSettings5Line className={styles.icon} />
-					<Link href="/setting">
-						<a className={styles.setting}>تنظیمات</a>
-					</Link>
-					<IoChevronBack className={styles.back} />
-				</div>
-
+				<Link href='/setting'>
+					<>
+						<div className={styles.box}>
+							<RiSettings5Line className={styles.icon} />
+							<a>تنظیمات</a>
+							<IoChevronBack className={styles.back} />
+						</div>
+					</>
+				</Link>
 				<div className={cn(styles.khat, 'col-11 offset-1')} />
-
-				<div className={styles.support}>
-					<BiSupport className={styles.icon} />
-					<Link href="/contact-us">
-						<a className={styles.support}>پشتیبانی</a>
-					</Link>
-					<IoChevronBack className={styles.back} />
-				</div>
+				<Link href='/contact-us'>
+					<>
+						<div className={styles.box}>
+							<BiSupport className={styles.icon} />
+							<a>پشتیبانی</a>
+							<IoChevronBack className={styles.back} />
+						</div>
+					</>
+				</Link>
 			</div>
 		</>
 	);
 };
 
+// <div className={cn(styles.khat, 'col-11 offset-1')} />
+// <div className={styles.setting}>
+// <RiSettings5Line className={styles.icon} />
+// <Link href='/setting'>
+// 	<a className={styles.setting}>تنظیمات</a>
+// </Link>
+// <IoChevronBack className={styles.back} />
+// </div>
+// <div className={cn(styles.khat, 'col-11 offset-1')} />
+// <div className={styles.support}>
+// <BiSupport className={styles.icon} />
+// <Link href='/contact-us'>
+// 	<a className={styles.support}>پشتیبانی</a>
+// </Link>
+// <IoChevronBack className={styles.back} />
+// </div>
 export default ProfileComp;
