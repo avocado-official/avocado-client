1c160e85ea1bf18ac7cbe657913ac5e33af3a276
Matin <matinkaboli@aol.com>
2021-10-09 01:49:22 +0330

    change index.js to index.jsx and move style file to the component file

---
+++ src/pages/setting/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Link from 'next/link';
+
+import styles from './setting.module.scss';
+import Footer from '../../components/footer';
+
+const Setting = () => (
+  <>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-12 col-md-12">
+          <div className={styles.headerBox}>
+            <p className={styles.header}>تنظیمات</p>
+          </div>
+
+          <Link href="/address">
+            <a>
+              <div className={styles.box}>
+                <a className={styles.link}>آدرس ها</a>
+              </div>
+            </a>
+          </Link>
+
+          <Link href="/delete-account">
+            <a>
+              <div className={styles.box}>
+                <a className={styles.link}>حذف حساب</a>
+              </div>
+            </a>
+          </Link>
+
+          <Footer />
+        </div>
+      </div>
+    </div>
+  </>
+);
+
+export default Setting;
